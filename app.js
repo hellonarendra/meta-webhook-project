e3ca52259b60d5cd2ac248a4c8898a4bc26ad3b8
Narendra verma <narendra.verma8005@gmail.com>
2023-07-24 11:11:05 +0530

    .

--- app.js
+++ app.js
@@ -99,101 +99,104 @@ app.post('/webhook', (req, res) => {
 });
 
 // Handles messages events
-function handleMessage(senderPsid, receivedMessage) {
-    let response;
+// function handleMessage(senderPsid, receivedMessage) {
+//     let response;
 
-    // Checks if the message contains text
-    if (receivedMessage.text) {
-        // Create the payload for a basic text message, which
-        // will be added to the body of your request to the Send API
-        response = {
-            'text': `You sent the message: '${receivedMessage.text}'. Now send me an attachment!`
-        };
-    } else if (receivedMessage.attachments) {
+//     // Checks if the message contains text
+//     if (receivedMessage.text) {
+//         // Create the payload for a basic text message, which
+//         // will be added to the body of your request to the Send API
+//         response = {
+//             'text': `You sent the message: '${receivedMessage.text}'. Now send me an attachment!`
+//         };
+//     } else if (receivedMessage.attachments) {
 
-        // Get the URL of the message attachment
-        let attachmentUrl = receivedMessage.attachments[0].payload.url;
-        response = {
-            'attachment': {
-                'type': 'template',
-                'payload': {
-                    'template_type': 'generic',
-                    'elements': [{
-                        'title': 'Is this the right picture?',
-                        'subtitle': 'Tap a button to answer.',
-                        'image_url': attachmentUrl,
-                        'buttons': [
-                            {
-                                'type': 'postback',
-                                'title': 'Yes!',
-                                'payload': 'yes',
-                            },
-                            {
-                                'type': 'postback',
-                                'title': 'No!',
-                                'payload': 'no',
-                            }
-                        ],
-                    }]
-                }
-            }
-        };
-    }
+//         // Get the URL of the message attachment
+//         let attachmentUrl = receivedMessage.attachments[0].payload.url;
+//         response = {
+//             'attachment': {
+//                 'type': 'template',
+//                 'payload': {
+//                     'template_type': 'generic',
+//                     'elements': [{
+//                         'title': 'Is this the right picture?',
+//                         'subtitle': 'Tap a button to answer.',
+//                         'image_url': attachmentUrl,
+//                         'buttons': [
+//                             {
+//                                 'type': 'postback',
+//                                 'title': 'Yes!',
+//                                 'payload': 'yes',
+//                             },
+//                             {
+//                                 'type': 'postback',
+//                                 'title': 'No!',
+//                                 'payload': 'no',
+//                             }
+//                         ],
+//                     }]
+//                 }
+//             }
+//         };
+//     }
 
-    // Send the response message
-    callSendAPI(senderPsid, response);
-}
+//     // Send the response message
+//     callSendAPI(senderPsid, response);
+// }
 
 // Handles messaging_postbacks events
-function handlePostback(senderPsid, receivedPostback) {
-    let response;
+// function handlePostback(senderPsid, receivedPostback) {
+//     let response;
 
-    // Get the payload for the postback
-    let payload = receivedPostback.payload;
+//     // Get the payload for the postback
+//     let payload = receivedPostback.payload;
 
-    // Set the response based on the postback payload
-    if (payload === 'yes') {
-        response = { 'text': 'Thanks!' };
-    } else if (payload === 'no') {
-        response = { 'text': 'Oops, try sending another image.' };
-    }
-    // Send the message to acknowledge the postback
-    callSendAPI(senderPsid, response);
-}
+//     // Set the response based on the postback payload
+//     if (payload === 'yes') {
+//         response = { 'text': 'Thanks!' };
+//     } else if (payload === 'no') {
+//         response = { 'text': 'Oops, try sending another image.' };
+//     }
+//     // Send the message to acknowledge the postback
+//     callSendAPI(senderPsid, response);
+// }
 
 // Sends response messages via the Send API
-function callSendAPI(senderPsid, response) {
+// function callSendAPI(senderPsid, response) {
 
-    // The page access token we have generated in your app settings
-    const PAGE_ACCESS_TOKEN = process.env.META_PAGE_ACCESS_TOKEN;
+//     // The page access token we have generated in your app settings
+//     const PAGE_ACCESS_TOKEN = process.env.META_PAGE_ACCESS_TOKEN;
 
-    // Construct the message body
-    let requestBody = {
-        'recipient': {
-            'id': senderPsid
-        },
-        'message': response
-    };
+//     // Construct the message body
+//     let requestBody = {
+//         'recipient': {
+//             'id': senderPsid
+//         },
+//         'message': response
+//     };
 
-    // Send the HTTP request to the Messenger Platform
-    request({
-        'uri': 'https://graph.facebook.com/v2.6/me/messages',
-        'qs': { 'access_token': PAGE_ACCESS_TOKEN },
-        'method': 'POST',
-        'json': requestBody
-    }, (err, _res, _body) => {
-        if (!err) {
-            console.log('Message sent!');
-        } else {
-            console.error('Unable to send message:' + err);
-        }
-    });
-}
+//     // Send the HTTP request to the Messenger Platform
+//     request({
+//         'uri': 'https://graph.facebook.com/v2.6/me/messages',
+//         'qs': { 'access_token': PAGE_ACCESS_TOKEN },
+//         'method': 'POST',
+//         'json': requestBody
+//     }, (err, _res, _body) => {
+//         if (!err) {
+//             console.log('Message sent!');
+//         } else {
+//             console.error('Unable to send message:' + err);
+//         }
+//     });
+// }
 
 function handleMessage(sender_psid, received_message) {
     console.log("Handle Message function call");
     const PAGE_ID = process.env.PAGE_ID;
+    console.log("page_id: ", PAGE_ID)
     const PAGE_ACCESS_TOKEN = process.env.META_PAGE_ACCESS_TOKEN;
+    console.log("PAGE_ACCESS_TOKEN: ", PAGE_ACCESS_TOKEN);
+    console.log("PSID: ", sender_psid);
     // PSID=6348126255224814
     // axios({
     //     method: "POST",
