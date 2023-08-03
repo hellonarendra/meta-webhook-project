d82371933d7e1cdaed13ad0926a96823b7508401
Narendra verma <narendra.verma8005@gmail.com>
2023-08-03 11:11:48 +0530

    added type script

--- app.js
+++ app.js
@@ -7,21 +7,18 @@ const axios = require('axios');
 
 'use strict';
 
-// Use dotenv to read .env vars into Node
-require('dotenv').config();
+require('dotenv').config();     // Use dotenv to read .env vars into Node
 
 // Imports dependencies and set up http server
 const
-    request = require('request'),
+request = require('request'),
     express = require('express'),
     { urlencoded, json } = require('body-parser'),
     app = express();
 
-// Parse application/x-www-form-urlencoded
-app.use(urlencoded({ extended: true }));
+app.use(urlencoded({ extended: true }));    // Parse application/x-www-form-urlencoded
 
-// Parse application/json
-app.use(json());
+app.use(json());    // Parse application/json
 
 // Respond with 'Hello World' when a GET request is made to the homepage
 app.get('/', function (_req, res) {
@@ -30,33 +27,23 @@ app.get('/', function (_req, res) {
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
-
     // Your verify token. Should be a random string.
     const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-
     // Parse the query params
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
-
     // Checks if a token and mode is in the query string of the request
     if (mode && token) {
-
         // Checks the mode and token sent is correct
         if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-            // Responds with the challenge token from the request
-            console.log('WEBHOOK_VERIFIED');
+            console.log('WEBHOOK_VERIFIED'); // Responds with the challenge token from the request
             res.status(200).send(challenge);
-
         } else {
-            // Responds with '403 Forbidden' if verify tokens do not match
-            res.sendStatus(403);
+            res.sendStatus(403);        // Responds with '403 Forbidden' if verify tokens do not match
         }
     }
 });
-
-
 
 // Creates the endpoint for your webhook
 app.post('/webhook', (req, res) => {
@@ -66,21 +53,13 @@ app.post('/webhook', (req, res) => {
     console.log("body from post API call: ", body);
     // Checks if this is an event from a page subscription
     if (body.object === 'page') {
-
         // Iterates over each entry - there may be multiple if batched
-
-        body.entry.forEach(function (entry) {
-
-            // Gets the body of the webhook event
-            let webhookEvent = entry.messaging[0];
+        body.entry.forEach(function (entry) {            
+            let webhookEvent = entry.messaging[0];          // Gets the body of the webhook event
             console.log(webhookEvent);
-
-            // Get the sender PSID
-            let senderPsid = webhookEvent.sender.id;
+            let senderPsid = webhookEvent.sender.id;        // Get the sender PSID
             console.log('Sender PSID: ' + senderPsid);
-
-            // Check if the event is a message or postback and
-            // pass the event to the appropriate handler function
+            // Check if the event is a message or postback and pass the event to the appropriate handler function
             if (webhookEvent.message) {
                 handleMessage(senderPsid, webhookEvent.message);
             } else if (webhookEvent.postback) {
@@ -88,13 +67,9 @@ app.post('/webhook', (req, res) => {
             }
         }
         );
-
-        // Returns a '200 OK' response to all requests
-        res.status(200).send('EVENT_RECEIVED');
+        res.status(200).send('EVENT_RECEIVED');     // Returns a '200 OK' response to all requests
     } else {
-
-        // Returns a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
+        res.sendStatus(404);        // Returns a '404 Not Found' if event is not from a page subscription
     }
 });
 
